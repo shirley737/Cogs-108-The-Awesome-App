623c72ab35dd9274e60c15ca0c3bcc42d9f1be3b
Emi Oda <emoda@ucsd.edu>
2019-02-13 20:43:11 -0800

    new update

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.get('/buySell', index.buySell)
 app.get('/buySell2', index.buySell2)
 app.get('/buySell3', index.buySell3)
 app.get('/buySell4', index.buySell4)
+app.get('/buySell5', index.buySell5)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
