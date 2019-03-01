c47ad8431b8cb7b3bbbb3ff8828c14c43e4502e0
Emi Oda <emoda@ucsd.edu>
2019-02-28 18:09:58 -0800

    page B done

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.get('/message1', index.message1)
 app.get('/message2', index.message2)
 app.get('/message3', index.message3)
 app.get('/message4', index.message4)
+app.get('/buySellPageB', index.buySellPageB)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
